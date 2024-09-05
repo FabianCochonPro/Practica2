fbd0b8f48b48a41f5c1cfc5f9a85cadb56f8899c
SebastianK <167713089+FabianCochonPro@users.noreply.github.com>
2024-09-05 07:49:50 -0500

    Update csdcd.cpp

--- csdcd.cpp
+++ csdcd.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "account.h"
+#include <string>
 using namespace std;
 int main()
 {
     Account account1{"Sebas", 51};
     Account account2 {"Blue", -1};
+    std::cout<<"account1:"<<account.getName<<"Balance is $"<<account1.getBalance();
+    std::cout<<"account2:"<<account.getName<<"Balance is $"<<account2.getBalance();
+    cout<<"\n\nEnter deposit amount for account1: "; //prompt
+    int depositAmount;
+    cin>>"adding<<depositAmount<
+
 }
